2550bdc6e073a1e6b8a494420b9b1f75abf373eb
Yohannes14 <yohansdegu.2010@gmail.com>
2023-07-16 18:06:25 +0300

    job details

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
 
-import styles from './company.style'
+import styles from "./company.style";
 
-const Company = () => {
+const Company = ({ companyLogo, companyTitle, companyName, location }) => {
   return (
     <View>
       <Text>Company</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Company
+export default Company;
