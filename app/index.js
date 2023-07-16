e17347834f6af6f99bcf042dc9d209e6ad8bab13
Yohannes14 <yohansdegu.2010@gmail.com>
2023-07-16 17:31:45 +0300

    job search

---
+++ app/index.js
@@ -0,0 +1,39 @@
+import { Stack, useRouter } from "expo-router";
+import { SafeAreaView, StyleSheet, Text, View, ScrollView } from "react-native";
+import { COLORS, SIZES, icons, images } from "../constants";
+import ScreenHeaderBtn from "../components/common/header/ScreenHeaderBtn";
+import Welcome from "../components/home/welcome/Welcome";
+import Popularjobs from "../components/home/popular/Popularjobs";
+import Nearbyjobs from "../components/home/nearby/Nearbyjobs";
+
+const Home = () => {
+  const router = useRouter();
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View> 
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
---
+++ hook/useFetch.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": "c170f98788msh5e8fd40bd9f6648p17a1e5jsn2c74f7fb353c",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.request(options);
+      setData(response?.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert("There is an error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
